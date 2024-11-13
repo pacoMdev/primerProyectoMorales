5e1c261415e701d546d64d342f0c110f87b74bd7
OsirisOsmium <fraciscomorales02@gmail.com>
2024-11-13 17:47:31 +0100

    nada

--- app/models/User.php
+++ app/models/User.php
@@ -25,19 +25,19 @@ class User{
 
     protected $surname;
 
-    // public function __construct($user_id, $email, $apple_id, $password, $phone, $direccion, $poblacion, $ciudad, $date_modification, $date_creation, $name, $surname)
-    // {
-    //     $this->user_id = $user_id;
-    //     $this->email = $email;
-    //     $this->apple_id = $apple_id;
-    //     $this->password = $password;
-    //     $this->phone = $phone;
-    //     $this->direccion = $direccion;
-    //     $this->poblacion = $poblacion;
-    //     $this->ciudad = $ciudad;
-    //     $this->date_modification = $date_modification;
-    //     $this->date_creation = $date_creation;
-    //     $this->name = $name;
-    //     $this->surname = $surname;
-    // }
+    public function __construct($user_id, $email, $apple_id, $password, $phone, $direccion, $poblacion, $ciudad, $date_modification, $date_creation, $name, $surname)
+    {
+        $this->user_id = $user_id;
+        $this->email = $email;
+        $this->apple_id = $apple_id;
+        $this->password = $password;
+        $this->phone = $phone;
+        $this->direccion = $direccion;
+        $this->poblacion = $poblacion;
+        $this->ciudad = $ciudad;
+        $this->date_modification = $date_modification;
+        $this->date_creation = $date_creation;
+        $this->name = $name;
+        $this->surname = $surname;
+    }
 }
